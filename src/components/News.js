a93e9fa04d8623752066648903d63e0f849a2677
Abhishek0090 <palabhishek411@gmail.com>
2021-09-06 18:32:15 +0530

    React router setup

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import propTypes from 'prop-types'
 
 export class News extends Component {
+
+    static defaultProps = {
+      country : 'in',
+      pageSize : 8,
+      category : 'general'
+    }
+
+    static propTypes = {
+      country :  propTypes.string,
+      pageSize : propTypes.number,
+      category : propTypes.string
+    }
+
   constructor() {
     super();
     console.log("Hello i m a constructor from news component");
@@ -14,7 +28,7 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8a13591bc28d493582ee0fed01760d2e&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -27,7 +41,7 @@ export class News extends Component {
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
@@ -48,7 +62,7 @@ export class News extends Component {
         Math.ceil(this.state.totalResults / this.props.pageSize)
       )
     ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
       this.setState({ loading: true });
@@ -64,8 +78,8 @@ export class News extends Component {
 
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center">NewsVilla - Top headlines!</h1>
+      <div className="container my-4">
+        <h1 className="text-center">NewsVilla &darr;   Top headlines and Updates </h1>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export default class NewsItem extends Component {
     let { title, description, imageUrl,newsUrl} = this.props; //setting our props
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={!imageUrl?"https://1721181113.rsc.cdn77.org/data/images/full/34888/science-times-twin-meteor-showers-dont-miss-this-weeks-occurrence-in-the-sky-that-could-produce-fireballs.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}..</h5>
-            <p className="card-text">{description}..</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">
               Read More
             </a>
