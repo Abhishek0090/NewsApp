aa293b00bc04484e2becb4c181382e392d12e089
Abhishek0090 <palabhishek411@gmail.com>
2021-09-06 09:58:14 +0530

    latest changes

--- src/components/News.js
+++ src/components/News.js
@@ -1,66 +1,110 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
-    articles = [
-        {
-          "source": { "id": "news24", "name": "News24" },
-          "author": "Lloyd Burnard",
-          "title": "Proteas go down in opening Sri Lanka ODI after losing skipper Bavuma to injury | Sport",
-          "description": "South African cricket so desperately needed a win, but it did not come in the first ODI between the Proteas and Sri Lanka in Colombo on Thursday.",
-          "url": "https://www.news24.com/sport/Cricket/Proteas/proteas-go-down-in-opening-sri-lanka-odi-as-skipper-bavuma-injured-20210902",
-          "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/11538/05ee13176ec84fa2947c8f8f9c214d2b.jpg",
-          "publishedAt": "2021-09-02T19:54:26+00:00",
-          "content": "South African cricket so desperately needed a win, but it did not come in the first ODI between the Proteas and Sri Lanka in Colombo on Thursday. \r\nThe Proteas were chasing 301 for victory after the … [+2164 chars]"
-        },
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-      ]
-    
-    constructor(){
-        super();
-        console.log("Hello i m a constructor from news component")
-        this.state = {
-            articles : this.articles,
-            loading : false
-        }
+  constructor() {
+    super();
+    console.log("Hello i m a constructor from news component");
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8a13591bc28d493582ee0fed01760d2e&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({ loading: false });
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
     }
-    render() {
-        return (
-            <div className="container my-3">
-                <h2>NewsMonkey - Top headlines{this.state.loading}</h2>
-                <div className="row">
-                    <div className="col-md-3">
-                    <NewsItem title="Cricket" description="Trying my first application" imageUrl="https://cdn.24.co.za/files/Cms/General/d/11538/05ee13176ec84fa2947c8f8f9c214d2b.jpg" newsUrl="TODO"/>
-                    </div>
-                    <div className="col-md-3">
-                    <NewsItem title="Cricket" description="Trying my first application"/>
-                    </div>
-                    <div className="col-md-3">
-                    <NewsItem title="Cricket" description="Trying my first application"/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h1 className="text-center">NewsVilla - Top headlines!</h1>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3 mx-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default News
+export default News;
